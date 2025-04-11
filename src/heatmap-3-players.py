c09f569feb5ac77fd28e0985637b4e2d682db302
Danilo Xavier <daniloxavier97@gmail.com>
2025-04-11 10:24:01 -0300

    Add heatmap by 3 players

---
+++ src/heatmap-3-players.py
@@ -0,0 +1,46 @@
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+import cv2
+
+# Configurações
+CSV_FILE = 'csv/tracking-high.csv'
+HEATMAP_IMAGE = 'img/heatmap_three_players'
+
+# Ler os dados
+df = pd.read_csv(CSV_FILE)
+
+# Usar centro da caixa como posição do jogador
+df['x_center'] = (df['x1'] + df['x2']) / 2
+df['y_center'] = (df['y1'] + df['y2']) / 2
+
+f = cv2.imread('img/frame_medio.png')
+frame_rgb = cv2.cvtColor(f, cv2.COLOR_BGR2RGB)
+h, w, _ = f.shape
+
+# Tamanho da figura com 3 subplots lado a lado
+player_ids = [3, 4, 9]
+plt.figure(figsize=(18, 6))
+
+for i, player_id in enumerate(player_ids):
+    plt.subplot(1, 3, i+1)
+    plt.imshow(frame_rgb)
+    
+    sns.kdeplot(
+        data=df[df['id'] == player_id],
+        x='x_center',
+        y='y_center',
+        fill=True,
+        cmap='viridis',  
+        alpha=0.6,
+        thresh=0.01,
+        levels=100
+    )
+    plt.title(f"Jogador ID {player_id}")
+    plt.axis('off')
+
+plt.suptitle("Mapas de Calor por Jogador", fontsize=16)
+plt.tight_layout()
+plt.subplots_adjust(top=0.88)  # Ajuste para não sobrepor o título
+plt.savefig(f"{HEATMAP_IMAGE}.png", dpi=300)
+plt.show()
